877f50e1076db6da07d9c45efc9285a07ac00a74
shahana <shahnazshah144@gmail.com>
2023-05-17 16:59:13 +0530

    hvjhvc

---
+++ movie_project/movie_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class movies (models.Model):
+    name=models.CharField(max_length=100)
+    desc=models.TextField()
+    year=models.IntegerField()
+
+    img=models.ImageField(upload_to='gallery')
+
+
+    def __str__(self):
+     return self.name
+
+
+
+
+
